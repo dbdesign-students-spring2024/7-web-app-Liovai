fbcb9c771a56eeda89b5e19e9b129d73607763aa
Liovai <sh6382@nyu.edu>
2024-04-19 18:56:53 -0400

    d

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# Flask-MongoDB Web App
+# Supermarket Inventory Manager
 
-In this assignment you will create a web app that relies upon a MongoDB database.
+## Description
 
-Replace the contents of this file with a description of your own web app, as described in [the instructions](./instructions.md).
+Supermarket Inventory Manager is a web-based application designed to help supermarket staff manage inventory efficiently. With this app, users can add new inventory items, view a list of all items, edit existing items, and remove items from the inventory. The app's intuitive interface makes inventory management straightforward and hassle-free.
+
+## Features
+
+- **Add New Items**: Easily input new stock items along with their prices and quantities.
+- **View Inventory**: Browse through the list of all inventory items in a neatly organized table.
+- **Edit Item Details**: Keep inventory information up to date by editing item entries.
+- **Delete Items**: Remove items that are no longer available from the inventory list.
+
+## Link to the deployed copy of your app
+Since I can't connect to i6 sever but it works on my local computer, here is the screenshot of web pages.
+
+This is the home page, providing brief explanations of the app and its functions.
+![homepage](images/home.png)
+
+This is the "add item" page, which users can freely add inventories of the market. Click *Create!* to add the item to the table, and click *Back to Home* to go back to the home page.
+![addpage](images/add.png)
+
+This is the "view inventory" page, which users can browse all inventories in the market. Click *Edit* to change the name, price and quantity of the items and click *Delete* to delete the specific item. Click *Back to Home* to go back to the home page.
+![inventorypage](images/inventory.png)
--- app.py
+++ app.py
@@ -1,204 +1,62 @@
-#!/usr/bin/env python3
-
-import os
-import sys
-import subprocess
-import datetime
-
-from flask import Flask, render_template, request, redirect, url_for, make_response
-
-# import logging
-import sentry_sdk
-from sentry_sdk.integrations.flask import (
-    FlaskIntegration,
-)  # delete this if not using sentry.io
-
-# from markupsafe import escape
-import pymongo
-from pymongo.errors import ConnectionFailure
-from bson.objectid import ObjectId
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson import ObjectId
 from dotenv import load_dotenv
+import os
 
-# load credentials and configuration options from .env file
-# if you do not yet have a file named .env, make one based on the template in env.example
-load_dotenv(override=True)  # take environment variables from .env.
+load_dotenv()  # Load environment variables from .env file.
 
-# initialize Sentry for help debugging... this requires an account on sentrio.io
-# you will need to set the SENTRY_DSN environment variable to the value provided by Sentry
-# delete this if not using sentry.io
-sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN"),
-    # enable_tracing=True,
-    # Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100% of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-    integrations=[FlaskIntegration()],
-    traces_sample_rate=1.0,
-    send_default_pii=True,
-)
-
-# instantiate the app using sentry for debugging
 app = Flask(__name__)
 
-# # turn on debugging if in development mode
-# app.debug = True if os.getenv("FLASK_ENV", "development") == "development" else False
+# Set up MongoDB connection
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client[os.getenv("MONGO_DBNAME")]
+collection = db['ex']  # Assuming you have a collection called 'items'
 
-# try to connect to the database, and quit if it doesn't work
-try:
-    cxn = pymongo.MongoClient(os.getenv("MONGO_URI"))
-    db = cxn[os.getenv("MONGO_DBNAME")]  # store a reference to the selected database
+@app.route('/')
+def index():
+    """Route to list all items - serves as the 'read' operation."""
+    items = collection.find()
+    return render_template('index.html', items=items)
 
-    # verify the connection works by pinging the database
-    cxn.admin.command("ping")  # The ping command is cheap and does not require auth.
-    print(" * Connected to MongoDB!")  # if we get here, the connection worked!
-except ConnectionFailure as e:
-    # catch any database errors
-    # the ping command failed, so the connection is not available.
-    print(" * MongoDB connection error:", e)  # debug
-    sentry_sdk.capture_exception(e)  # send the error to sentry.io. delete if not using
-    sys.exit(1)  # this is a catastrophic error, so no reason to continue to live
-
-
-# set up the routes
-
-
-@app.route("/")
-def home():
-    """
-    Route for the home page.
-    Simply returns to the browser the content of the index.html file located in the templates folder.
-    """
-    return render_template("index.html")
-
-
-@app.route("/read")
-def read():
-    """
-    Route for GET requests to the read page.
-    Displays some information for the user with links to other pages.
-    """
-    docs = db.exampleapp.find({}).sort(
-        "created_at", -1
-    )  # sort in descending order of created_at timestamp
-    return render_template("read.html", docs=docs)  # render the read template
-
-
-@app.route("/create")
+@app.route('/create', methods=['GET', 'POST'])
 def create():
-    """
-    Route for GET requests to the create page.
-    Displays a form users can fill out to create a new document.
-    """
-    return render_template("create.html")  # render the create template
+    """Route to show the form and create a new item - serves as the 'create' operation."""
+    if request.method == 'POST':
+        item_data = {
+            'name': request.form['name'],
+            'price': request.form['price'],
+            'quantity': request.form['quantity']
+        }
+        collection.insert_one(item_data)
+        return redirect(url_for('index'))
+    return render_template('create.html')
 
+@app.route('/inventory')
+def inventory():
+    """Route to list all items."""
+    items = collection.find()
+    return render_template('inventory.html', items=items)
 
-@app.route("/create", methods=["POST"])
-def create_post():
-    """
-    Route for POST requests to the create page.
-    Accepts the form submission data for a new document and saves the document to the database.
-    """
-    name = request.form["fname"]
-    message = request.form["fmessage"]
+@app.route('/edit/<item_id>', methods=['GET', 'POST'])
+def edit(item_id):
+    """Route to edit an item - serves as the 'update' operation."""
+    item_to_edit = collection.find_one({'_id': ObjectId(item_id)})
+    if request.method == 'POST':
+        updated_data = {
+            'name': request.form['name'],
+            'price': request.form['price'],
+            'quantity': request.form['quantity']
+        }
+        collection.update_one({'_id': ObjectId(item_id)}, {'$set': updated_data})
+        return redirect(url_for('index'))
+    return render_template('edit.html', item=item_to_edit)
 
-    # create a new document with the data the user entered
-    doc = {"name": name, "message": message, "created_at": datetime.datetime.utcnow()}
-    db.exampleapp.insert_one(doc)  # insert a new document
+@app.route('/delete/<item_id>')
+def delete(item_id):
+    """Route to delete an item."""
+    collection.delete_one({'_id': ObjectId(item_id)})
+    return redirect(url_for('index'))
 
-    return redirect(
-        url_for("read")
-    )  # tell the browser to make a request for the /read route
-
-
-@app.route("/edit/<mongoid>")
-def edit(mongoid):
-    """
-    Route for GET requests to the edit page.
-    Displays a form users can fill out to edit an existing record.
-
-    Parameters:
-    mongoid (str): The MongoDB ObjectId of the record to be edited.
-    """
-    doc = db.exampleapp.find_one({"_id": ObjectId(mongoid)})
-    return render_template(
-        "edit.html", mongoid=mongoid, doc=doc
-    )  # render the edit template
-
-
-@app.route("/edit/<mongoid>", methods=["POST"])
-def edit_post(mongoid):
-    """
-    Route for POST requests to the edit page.
-    Accepts the form submission data for the specified document and updates the document in the database.
-
-    Parameters:
-    mongoid (str): The MongoDB ObjectId of the record to be edited.
-    """
-    name = request.form["fname"]
-    message = request.form["fmessage"]
-
-    doc = {
-        # "_id": ObjectId(mongoid),
-        "name": name,
-        "message": message,
-        "created_at": datetime.datetime.utcnow(),
-    }
-
-    db.exampleapp.update_one(
-        {"_id": ObjectId(mongoid)}, {"$set": doc}  # match criteria
-    )
-
-    return redirect(
-        url_for("read")
-    )  # tell the browser to make a request for the /read route
-
-
-@app.route("/delete/<mongoid>")
-def delete(mongoid):
-    """
-    Route for GET requests to the delete page.
-    Deletes the specified record from the database, and then redirects the browser to the read page.
-
-    Parameters:
-    mongoid (str): The MongoDB ObjectId of the record to be deleted.
-    """
-    db.exampleapp.delete_one({"_id": ObjectId(mongoid)})
-    return redirect(
-        url_for("read")
-    )  # tell the web browser to make a request for the /read route.
-
-
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    """
-    GitHub can be configured such that each time a push is made to a repository, GitHub will make a request to a particular web URL... this is called a webhook.
-    This function is set up such that if the /webhook route is requested, Python will execute a git pull command from the command line to update this app's codebase.
-    You will need to configure your own repository to have a webhook that requests this route in GitHub's settings.
-    Note that this webhook does do any verification that the request is coming from GitHub... this should be added in a production environment.
-    """
-    # run a git pull command
-    process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
-    pull_output = process.communicate()[0]
-    # pull_output = str(pull_output).strip() # remove whitespace
-    process = subprocess.Popen(["chmod", "a+x", "flask.cgi"], stdout=subprocess.PIPE)
-    chmod_output = process.communicate()[0]
-    # send a success response
-    response = make_response(f"output: {pull_output}", 200)
-    response.mimetype = "text/plain"
-    return response
-
-
-@app.errorhandler(Exception)
-def handle_error(e):
-    """
-    Output any errors - good for debugging.
-    """
-    return render_template("error.html", error=e)  # render the edit template
-
-
-# run the app
-if __name__ == "__main__":
-    # logging.basicConfig(filename="./flask_error.log", level=logging.DEBUG)
-    app.run(load_dotenv=True)
+if __name__ == '__main__':
+    app.run(debug=True)
